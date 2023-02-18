df6695021c08153d726382991e602d7903b0fb07
Aziz <aziznal.dev@gmail.com>
2023-02-18 19:44:23 +0300

    setup router and main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.scss";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-
-function App() {
+// returns a hello world screen
+export default function MainScreen() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
     </div>
   );
 }
-
-export default App;
